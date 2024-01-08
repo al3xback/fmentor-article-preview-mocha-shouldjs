fcbddfead5f00940d77a00e68824868ac08a1287
alex <al3xback@gmail.com>
2024-01-08 14:38:31 +0700

    added first commit

---
+++ test/main.js
@@ -0,0 +1,55 @@
+import should from 'should';
+import jsdom from 'jsdom';
+import got from 'got';
+
+const { JSDOM } = jsdom;
+
+const url = 'https://al3xback.github.io/fmentor-article-preview-flex/';
+
+const getData = () => {
+	return got(url)
+		.then((res) => {
+			const { document } = new JSDOM(res.body).window;
+			return document;
+		})
+		.catch((err) => {
+			throw new Error(err);
+		});
+};
+
+describe('DOM', () => {
+	beforeEach(async () => {
+		try {
+			const document = await getData();
+			global.document = document;
+		} catch (err) {
+			console.log(err);
+		}
+	});
+
+	it('should have an empty alt attribute value of card drawer image element', () => {
+		const cardDrawerImageAlt =
+			document.querySelector('.card__image img').alt;
+
+		cardDrawerImageAlt.should.be.empty();
+	});
+
+	it("should have a post date element with a class of 'card__author-post-date'", () => {
+		const cardPostDateEl = document.querySelector(
+			'.card__author-post-date'
+		);
+
+		cardPostDateEl.should.be.ok();
+	});
+
+	it('should have a post date year ranging from 2018 to 2020', () => {
+		const cardPostDate = document.querySelector(
+			'.card__author-post-date'
+		).textContent;
+		const cardPostDateYear = cardPostDate
+			.split(' ')[2]
+			.replace(/\n|\t/g, '');
+
+		cardPostDateYear.should.be.within(2018, 2020);
+	});
+});
